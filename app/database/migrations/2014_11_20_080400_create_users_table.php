4e6a2fdd9d1a672758d5c82d3718bdd525448134
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-20 10:40:28 +0200

    Migration for creating users table

--- app/database/migrations/2014_11_20_080400_create_users_table.php
+++ app/database/migrations/2014_11_20_080400_create_users_table.php
@@ -14,8 +14,14 @@ class CreateUsersTable extends Migration {
         Schema::create('users', function(Blueprint $table) {
             $table->increments('id');
             $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('password');
-
+            $table->string('creation_ip');
+            $table->string('last_ip_accessed');
+            $table->integer('total_logins');
+            $table->string('account_picture')->unique();
             $table->timestamps();
         });
     }
